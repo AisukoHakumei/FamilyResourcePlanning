87d11254cb0e6532bb7db243b1fab73a866e824c
AisukoHakumei <aisuko@tekaido.com>
2025-05-16 21:49:07 +0200

    feat!: Switch to Sveltekit + Drizzle

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,6 @@
-import { defineConfig } from 'vite'
-import { getDirname } from '@adonisjs/core/helpers'
-import inertia from '@adonisjs/inertia/client'
-import vue from '@vitejs/plugin-vue'
-import adonisjs from '@adonisjs/vite/client'
-import tailwindcss from '@tailwindcss/vite'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.ts' } }), 
-    vue(), 
-    adonisjs({ entrypoints: ['inertia/app/app.ts'], reload: ['resources/views/**/*.edge'] })
-  ],
-
-  /**
-   * Define aliases for importing modules from
-   * your frontend code
-   */
-  resolve: {
-    alias: {
-      '~/': `${getDirname(import.meta.url)}/inertia/`,
-      '@': `${getDirname(import.meta.url)}/inertia/lib`
-    },
-  },
-})
+	plugins: [sveltekit()]
+});
