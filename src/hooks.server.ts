4ed872adf1a6ec6f51d5cac9a49094337c607e5a
AisukoHakumei <aisuko@tekaido.com>
2025-06-10 17:42:02 +0200

    feat: mock seeding data with auto-init

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,20 @@
 import type { Handle, ServerInit } from '@sveltejs/kit';
 import * as auth from '$lib/server/auth.js';
+import { execSync } from "node:child_process"
+import { existsSync } from 'node:fs';
+import { mainSeed } from '$lib/server/db/seed';
+import { DATABASE_URL } from '$env/static/private';
 
 export const init: ServerInit = async () => {
+
+	if (existsSync("local.db")) {
+		console.log("Database already exists, not doing anything...")
+	} else {
+		console.log("Creating database and seeding it...")
+		execSync("touch " + DATABASE_URL + " && pnpm run db:push --force")
+		await mainSeed()
+	}
+
 	console.log("Server ready...")
 }
 
--- src/lib/server/db/seed/index.ts
+++ src/lib/server/db/seed/index.ts
@@ -6,24 +6,57 @@ import * as familySchema from "../schema/family"
 import * as budgetSchema from "../schema/budget"
 
 
+export async function mainSeed() {
+    await db.insert(familySchema.family).values({
+        id: "test",
+        name: "DEV",
+    });
 
-export async function resetDatabase() {
-    try {
-    await reset(db, userSchema)
-    await reset(db, familySchema)
-    await reset(db, budgetSchema)
-    } catch {
-        console.log("Failed to reset database. Maybe it was empty...")
-    }
-}
+    await db.insert(userSchema.user).values({
+        id: "m747iuirivj3v34y4thrrjrm",
+        username: "test",
+        passwordHash: "$argon2id$v=19$m=19456,t=2,p=1$E+v+yWQjwFFf3nYoA4dYIQ$6X0+jvaQio6TcKLaeKfbNgcRldHRlqtL6zQKQ/8+SZA",
+        familyId: "test",
+    });
 
-export async function createDevFamily() {
-    try {
-        db.insert(familySchema.family).values({
-            id: "test",
-            name: "DEV",
-        })
-    } catch {
-        console.log("Failed to create dev family...")
-    }
+    await db.insert(budgetSchema.budget).values({
+        id: "budg1",
+        name: "Réaménagement du salon",
+        belongsTo: "test"
+    });
+
+    await db.insert(budgetSchema.budget).values({
+        id: "budg2",
+        name: "Réaménagement de la cuisine",
+        belongsTo: "test"
+    });
+
+    await db.insert(budgetSchema.budgetLine).values({
+        id: "bl1",
+        name: "Tables et chaises",
+        allocatedAmount: 100.0,
+        createdAt: new Date(Date.now()),
+        createdBy: "m747iuirivj3v34y4thrrjrm",
+        budgetId: "budg1"
+    });
+
+    await db.insert(budgetSchema.budgetLine).values({
+        id: "bl2",
+        name: "Tapis et coussins",
+        allocatedAmount: 76.0,
+        usedAmount: 20.0,
+        createdAt: new Date(Date.now()),
+        createdBy: "m747iuirivj3v34y4thrrjrm",
+        budgetId: "budg1"
+    });
+
+    await db.insert(budgetSchema.budgetLine).values({
+        id: "bl3",
+        name: "Couteaux de cuisine",
+        allocatedAmount: 760.0,
+        usedAmount: 200.0,
+        createdAt: new Date(Date.now()),
+        createdBy: "m747iuirivj3v34y4thrrjrm",
+        budgetId: "budg2"
+    });
 }
