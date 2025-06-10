20d186ef0cecc4d838474b097a04e006dce1d55a
AisukoHakumei <aisuko@tekaido.com>
2025-06-10 17:57:29 +0200

    chore: ajust right sidebar width and checkbox

--- src/lib/components/layout/calendars.svelte
+++ src/lib/components/layout/calendars.svelte
@@ -24,9 +24,7 @@
 						{#each calendar.items as item, index (item)}
 							<Sidebar.MenuItem>
 								<Sidebar.MenuButton>
-									<div data-active={index < 2} class="group/calendar-item border-sidebar-border text-sidebar-primary-foreground data-[active=true]:border-sidebar-primary data-[active=true]:bg-sidebar-primary flex aspect-square size-4 shrink-0 items-center justify-center rounded-sm border">
-										<Checkbox class="hidden size-3 group-data-[active=true]/calendar-item:block" />
-									</div>
+									<Checkbox />
 									{item}
 								</Sidebar.MenuButton>
 							</Sidebar.MenuItem>
